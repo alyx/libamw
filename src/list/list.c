e929f9b3cc30c9d3e588eac4cf1b1464c226008f
Alyx <alyx@malkier.net>
2012-01-07 15:25:00 -0500

    Remove test code

--- src/list/list.c
+++ src/list/list.c
@@ -1,8 +1,4 @@
-#include "list.h"
-#include <stdlib.h>
-#ifdef AMW_LIST_TEST
-#include <stdio.h>
-#endif
+#include "amw.h"
 
 amw_list_t *amw_list_new(void)
 {
@@ -120,61 +116,3 @@ void amw_list_shift(amw_list_t *list)
     amw_list_del(list, list->tail);
 }
 
-
-#ifdef AMW_LIST_TEST
-void amw_list_printlist(amw_list_t *list)
-{
-    amw_list_node *n;
-    n = list->head;
-    if (n == NULL)
-        printf("WTF? Head is NULL!\n");
-
-    while (n)
-    {
-        printf("%d ", *(int *)n->data);
-        n = n->next;
-    }
-    printf("\n");
-}
-
-void amw_list_printlist_backwards(amw_list_t *list)
-{
-    amw_list_node *n;
-    n = list->tail;
-    if (n == NULL)
-        printf("WTF? Tail is NULL!\n");
-
-    while (n)
-    {
-        printf("%d ", *(int *)n->data);
-        n = n->prev;
-    }
-    printf("\n");
-}
-
-int main(void)
-{
-    int i;
-    amw_list_t *testlist;
-    amw_list_node *n;
-    testlist = amw_list_new();
-    int data[] = {8,0,7,2,5,3,6,9,4,1};
-    for(i = 0; i < sizeof(data)/sizeof(data[0]); i++)
-    {
-        printf("Adding %d...\n", data[i]);
-        n = amw_list_new_node(&data[i]);
-        amw_list_append_node(testlist, n);
-    }
-    n = testlist->head->next->next;
-    printf("n: %d\n", *(int *)n->data);
-    amw_list_printlist(testlist);
-    amw_list_del(testlist, n);
-    printf("Forwards: ");
-    amw_list_printlist(testlist);
-    printf("Backwards: ");
-    amw_list_printlist_backwards(testlist);
-
-    return 0;
-}
-
-#endif
