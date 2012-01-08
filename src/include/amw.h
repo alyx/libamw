3f331237c7856b8410126f8e8150f889f5bafea5
Alyx <alyx@malkier.net>
2012-01-08 14:47:02 -0500

    Move libms code into libamw. Store i in a register for small (theoretical?) speed increase

--- src/include/amw.h
+++ src/include/amw.h
@@ -1,6 +1,7 @@
 #ifndef __AMW_H
 #define __AMW_H
 
+#include <inttypes.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
@@ -8,6 +9,7 @@
 
 #include "list.h"
 #include "misc.h"
+#include "prng.h"
 #include "timer.h"
 
 #endif /* __AMW_H */
---
+++ src/include/prng.h
@@ -0,0 +1,7 @@
+#ifndef __AMW_PRNG_H
+#define __AMW_PRNG_H
+
+void amw_prng_ms_init(uint32_t seed);
+uint16_t amw_prng_ms_get(void);
+
+#endif /* __AMW_PRNG_H */
